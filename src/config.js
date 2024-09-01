e193e7a9f12c93773fe4d061534959921d8cd273
Suke-H <hondii1997@gmail.com>
2024-09-01 13:09:27 +0900

    ⚙️ fix: 本番用スケジューラーの設定変更

--- src/config.js
+++ src/config.js
@@ -15,8 +15,10 @@ const config = {
   production: {
     // GOAL_SETTING_CRON: '0 9 * * 0',  // 毎週日曜日の午前9時
     // WEEKLY_REPORT_CRON: '0 17 * * 5' // 毎週金曜日の午後5時
-    GOAL_SETTING_CRON: '*/10 * * * *',   // 10分に1回
-    WEEKLY_REPORT_CRON: '*/10 * * * *'  // 10分に1回
+    GOAL_SETTING_CRON: '0 9 * * *',   // 毎日午前9時
+    WEEKLY_REPORT_CRON: '0 17 * * *'  // 毎日午後5時
+    // GOAL_SETTING_CRON: '*/10 * * * *',   // 10分に1回
+    // WEEKLY_REPORT_CRON: '*/10 * * * *'  // 10分に1回
   },
 };
 
